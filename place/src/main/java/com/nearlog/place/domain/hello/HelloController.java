a014f6a80af9cee0429b1da54bc721a58f4ac361
bgpark <bgpark82@gmail.com>
2020-10-24 23:07:46 +0900

    feat: Add HelloController

---
+++ place/src/main/java/com/nearlog/place/domain/hello/HelloController.java
@@ -0,0 +1,13 @@
+package com.nearlog.place.domain.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping
+    public String hello() {
+        return "place";
+    }
+}
