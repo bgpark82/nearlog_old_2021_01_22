c7ccf115cabea52c58d470c84fa61ad7ed4e902f
bgpark <bgpark82@gmail.com>
2020-10-27 00:51:35 +0900

    feat: configuring springboot-mysql connection in docker-compose

---
+++ user/src/main/java/com/nearlog/user/domain/user/UserController.java
@@ -0,0 +1,21 @@
+package com.nearlog.user.domain.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/user")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    @GetMapping
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+}
