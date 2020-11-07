f5ed4300567b9394456fdfe9ef5c1d72b0b728c1
bgpark <bgpark82@gmail.com>
2020-11-07 19:55:18 +0900

    feat: Update User domain

--- user/src/main/java/com/nearlog/user/domain/user/User.java
+++ user/src/main/java/com/nearlog/user/domain/user/User.java
@@ -1,16 +1,48 @@
 package com.nearlog.user.domain.user;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
+@ToString
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long id;
 
-    private String username;
+    private String email;
+    @ToString.Exclude
+    private String password;
+    private String nickname;
+    private String profile;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole;
+    @Enumerated(EnumType.STRING)
+    private JoinType joinType;
+    @Enumerated(EnumType.STRING)
+    private UserStatus status;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+
+    public enum UserRole {
+        USER, ADMIN
+    }
+
+    public enum JoinType {
+        GOOGLE
+    }
+
+    public enum UserStatus {
+        ENABLED, DISABLED,
+    }
 }
